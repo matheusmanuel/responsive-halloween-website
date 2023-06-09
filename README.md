2fd540408adb0217e38927b6f7559f858ed7258b
Matheus manuel <matheusmanuel111@gmail.com>
2023-06-09 16:01:38 +0100

    primeiro commit

---
+++ README.md
@@ -0,0 +1,60 @@
+<div align="center">
+    <h1 align="center">Create a Responsive Halloween Website Design Using HTML CSS & JavaScript 🎃</h1>
+    <p>Criação de um site com tema sobre Halloween para treino e fixação de conteúdo.</p>
+    <img src="./design/preview.png" alt="Logo" width="800">
+</div>
+
+---
+<h3 align="center">
+  <a href="https://responsive-halloween-website-2.vercel.app/">Acessar demonstração</a>
+</h3>
+
+## Índice
+
+* [Descrição](#descrição)
+* [Techs](#techs)
+* [Design](#design)
+  * [Cores](#cores)
+  * [Tipo de fonte](#tipo-de-fonte)
+  * [Pacote de ícones](#pacote-de-ícones)
+* [Instalação](#instalação)
+* [Links Contato](#links-contato)
+
+# Descrição
+Criação de um site com tema sobre Halloween para treino e fixação de conteúdo.[**@BedimCode**](https://www.youtube.com/c/Bedimcode)
+
+# Techs: 
+- **HTML**
+- **CSS**
+- **Javascript**
+
+# Design:
+- O modelo final para desktop está disponível na pasta `./design`
+- Imagens disponíveis na pasta `./assets`<br>
+
+## Cores:
+--first-color: hsl(22, 72%, 50%);<br>
+--first-color-alt: hsl(22, 72%, 46%);<br>
+--white-color: #fff;<br>
+--text-color: hsl(22, 4%, 75%);<br>
+--text-color-light: hsl(22, 4%, 55%);<br>
+--body-color: hsl(22, 8%, 6%);<br>
+--container-color: hsl(22, 8%, 7%);
+
+## Tipo de fonte:
+- **Poppins** Regular 400, Medium 500, Bold 700
+- **Butcherman**
+
+## Pacote de ícones:
+- **Boxicons**
+
+# Instalação:
+```bash
+  # Clone este repositório:
+  $ git clone https://github.com/GabrielChagas1/responsive-halloween-website.git
+  $ cd ./responsive-halloween-website
+```
+
+# Links Contato
+- **Linkedin:** https://www.linkedin.com/in/gabriel-serqueira-chagas/<br>
+- **GitHub:** https://github.com/GabrielChagas1<br>
---
+++ assets/js/main.js
@@ -0,0 +1,83 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader(){
+    const header = document.getElementById('header')
+    // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 80) header.classList.add('bg-header'); else header.classList.remove('bg-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*=============== SHOW SCROLL UP ===============*/ 
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 400 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 400) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    //reset: true //Animations repeat
+});
+
+sr.reveal(`.home__data, .footer__content, .footer__logo, .footer__description`)
+sr.reveal(`.home__tree-1`, {origin: 'left', delay: 800})
+sr.reveal(`.home__tree-2`, {origin: 'right', delay: 800})
+sr.reveal(`.home__img`, {delay: 800})
+sr.reveal(`.category__card, .items__card`, {interval: 100})
+sr.reveal(`.about__img, .about__data, .footer__tree-2`, {origin: 'left'})
+sr.reveal(`.party__images, .party__data, .footer__tree-1`, {origin: 'rigth'})
